fc903a363dad82dfcb9d43927a88e44d3b12c4e0
Viktor Yushkevich <viktor.yushkevich@vrpconsulting.com>
2020-08-31 13:28:21 +0300

    Updates

---
+++ precache-manifest.11ff6e7a6c602f2f1a226b4adcad706b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9818f80937a95c8d686d0f466d538d62",
+    "url": "/https//viktaryushkevich/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9070bb90f2ebf584aa26",
+    "url": "/https//viktaryushkevich/github.io/monsters-rolodex/static/css/main.89b89aef.chunk.css"
+  },
+  {
+    "revision": "17e7a3acfa6de3ea2ab4",
+    "url": "/https//viktaryushkevich/github.io/monsters-rolodex/static/js/2.faf01414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//viktaryushkevich/github.io/monsters-rolodex/static/js/2.faf01414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9070bb90f2ebf584aa26",
+    "url": "/https//viktaryushkevich/github.io/monsters-rolodex/static/js/main.ee820c08.chunk.js"
+  },
+  {
+    "revision": "fad4a3a4d511d48aeb48",
+    "url": "/https//viktaryushkevich/github.io/monsters-rolodex/static/js/runtime-main.ed8c13cb.js"
+  }
+]);
