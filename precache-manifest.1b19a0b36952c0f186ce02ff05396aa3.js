156403ec9ecd3648e93c817aa33038eef56b827c
Viktor Yushkevich <viktor.yushkevich@vrpconsulting.com>
2020-08-31 13:40:00 +0300

    Updates

---
+++ precache-manifest.1b19a0b36952c0f186ce02ff05396aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b92b30468c68ba4da0bb046e4ad337",
+    "url": "/https//github.io/viktaryushkevich/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1708b6d045391de74f67",
+    "url": "/https//github.io/viktaryushkevich/monsters-rolodex/static/css/main.89b89aef.chunk.css"
+  },
+  {
+    "revision": "17e7a3acfa6de3ea2ab4",
+    "url": "/https//github.io/viktaryushkevich/monsters-rolodex/static/js/2.faf01414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//github.io/viktaryushkevich/monsters-rolodex/static/js/2.faf01414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1708b6d045391de74f67",
+    "url": "/https//github.io/viktaryushkevich/monsters-rolodex/static/js/main.8cc02472.chunk.js"
+  },
+  {
+    "revision": "7fcfae1b7b6feba4373b",
+    "url": "/https//github.io/viktaryushkevich/monsters-rolodex/static/js/runtime-main.fbeaad91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https//viktaryushkevich.github.io/monsters-rolodex/precache-manifest.917df07be574be9ade25f954b801dc2e.js"
+  "/https//github.io/viktaryushkevich/monsters-rolodex/precache-manifest.1b19a0b36952c0f186ce02ff05396aa3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//viktaryushkevich.github.io/monsters-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//github.io/viktaryushkevich/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
